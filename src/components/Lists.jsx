18550d468ef763faa475e948d80c2af66ce71ea4
priyabratadas <pdas544@gmail.com>
2026-01-19 22:34:12 +0530

    add list component

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -2,7 +2,8 @@ const Lists = () => {
     const userInfo = [
         {name:"Priyabrata", email:"abc1@test.com"},
         {name:"John", email:"abc2@test.com"},
-        {name:"Hex", email:"abc3@test.com"}
+        {name:"Hex", email:"abc3@test.com"},
+        {name:"Harry", email: "abc4@test.com"}
     ];
     return <section>
         {userInfo.map(({name, email})=>
